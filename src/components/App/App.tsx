e1ba33a2c6083ea96639670bd003973b078e65b9
Irina Struy <irastruy@yandex.ru>
2024-04-18 16:05:21 +0300

    change App tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,8 +13,8 @@ export const App: FC = () => {
   useEffect(() => {
     fetch('https://api.github.com/users', {
       headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        "Accept": 'application/vnd.github+json',
+        "Authorization": `Bearer ${process.env.REACT_APP_API_KEY}`,
       },
     })
       .then((response) => response.json())
